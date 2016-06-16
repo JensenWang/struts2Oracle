aa0eddef715f94c475d40cda3b13e6ef8f361ad2
JensenWang <BlankInter@Jensen>
2016-06-16 19:51:53 +0800

    创建学表和学生照片表

---
+++ sql/xsb.sql
@@ -0,0 +1,11 @@
+-- Ñ§Éú±í
+create table xsb
+(
+	xh	char(6)	not null	primary key,
+	xm	char(8)	not null,
+	xb	char(2)	default '1'	not null,
+	cssj	date	not null,
+	zy	char(12)	null,
+	zxf	number(2)	default 0	check (zxf>=0 and zxf<160),
+	bz	varchar2(200)	null
+)
---
+++ sql/xszp.sql
@@ -0,0 +1,8 @@
+-- Ñ§ÉúÏàÆ¬
+create table xszp
+(
+	xh	char(6)	not null,
+	zp	blob	null,
+	foreign key (xh) references xsb(xh)
+	on delete cascade
+)
