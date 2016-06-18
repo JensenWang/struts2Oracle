60c8294fb7791471a49aab1fbb71082e60e4c3d5
JensenWang <BlankInter@Jensen>
2016-06-19 00:31:50 +0800

    create DBConnTest

---
+++ test/test/DBConnTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.jdbc.DBConn;
+import org.junit.Test;
+
+public class DBConnTest {
+	
+
+	@Test
+	public void testSaveStudenet() {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		conn = new DBConn().getConn();
+		String sql = "insert into xsb values('test','testName','1',TO_DATE('19950714','YYYYMMDD'),'testZy',30,'testBz')";
+		try {
+			ps=conn.prepareStatement(sql);
+			ps.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
