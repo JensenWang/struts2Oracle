dd502ff3f28917a64bc7d6d9f444b343b656a69a
汪志杰 <572927462@qq.com>
2016-06-24 17:14:50 +0800

    创建成绩表的vo对象

---
+++ src/org/vo/Score.java
@@ -0,0 +1,40 @@
+package org.vo;
+
+public class Score {
+
+    private String xh;  // 学号
+    private String kch; // 课程号
+    private String cj;  // 成绩
+    
+    public Score(String xh, String kch, String cj) {
+        super();
+        this.xh = xh;
+        this.kch = kch;
+        this.cj = cj;
+    }
+
+    public String getXh() {
+        return xh;
+    }
+
+    public void setXh(String xh) {
+        this.xh = xh;
+    }
+
+    public String getKch() {
+        return kch;
+    }
+
+    public void setKch(String kch) {
+        this.kch = kch;
+    }
+
+    public String getCj() {
+        return cj;
+    }
+
+    public void setCj(String cj) {
+        this.cj = cj;
+    }
+    
+}
